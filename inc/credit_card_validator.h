13ff41e8bbba5c80232d0478dc34dafe1963c312
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-05-02 20:13:24 +0200

    Added: chech_lenght_card_number() | card_number_verification() | split_the_number() | multiply_the_number() methode.

--- inc/credit_card_validator.h
+++ inc/credit_card_validator.h
@@ -1,4 +1,5 @@
 // ReSharper disable IdentifierTypo
+
 #pragma once
 
 #include <iostream>
@@ -7,10 +8,17 @@ class credit_card_validator
 {
 	public:
 
-		bool start_luhn_algorithm(int number);
+		bool start_luhn_algorithm(const int card_number);
 
 	private:
 
-			
+		std::string credit_card_number_;
+		int splited_number_[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		const int multiply_numbers_[10] = { 0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+
+		bool check_lenght_card_number() const;
+		bool card_number_verification();
+		void split_the_number();
+		void multiply_the_number();
 
 };
--- src/credit_card_validator.cpp
+++ src/credit_card_validator.cpp
@@ -1,7 +1,58 @@
 // ReSharper disable IdentifierTypo
-#include "credit_card_validator.h"
 
-bool credit_card_validator::start_luhn_algorithm(int number)
+#include "credit_card_validator.h"
+#include "string"
+
+bool credit_card_validator::start_luhn_algorithm(const int card_number)
 {
+	credit_card_number_ = std::to_string(card_number);
+	if (check_lenght_card_number() && card_number_verification())
+	{
+		return true;
+	}
+	return false;
+}
+
+bool credit_card_validator::check_lenght_card_number() const
+{
+	if (credit_card_number_.length() == 16)
+	{
+		return true;
+	}
+	system("cls");
+	std::cout << "Uncorrected credit card length.\n";
+	return false;
+}
+
+bool credit_card_validator::card_number_verification()
+{
+	split_the_number();
+	multiply_the_number();
 	return true;
+}
+
+void credit_card_validator::split_the_number()
+{
+	for (int i = 0; i < 16; i++)
+	{
+		splited_number_[i] = static_cast<int>(credit_card_number_[i]);
+	}
+}
+
+void credit_card_validator::multiply_the_number()
+{
+	for (int i = 15; i >= 0; i--)
+	{
+		if (i % 2 == 0)
+		{
+			for (int y = 0; y < 9; y++)
+			{
+				if (splited_number_[i] == y)
+				{
+					splited_number_[i] = multiply_numbers_[y];
+					break;
+				}
+			}
+		}
+	}
 }
